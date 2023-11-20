1022cbe925a6079cbe52a1cd176e7f52481884de
SethBowman <brandonsethbowman@gmail.com>
2023-11-20 16:46:59 -0600

    added styling

--- Fallout76Apparel/Controllers/HomeController.cs
+++ Fallout76Apparel/Controllers/HomeController.cs
@@ -24,11 +24,11 @@ namespace Fallout76Apparel.Controllers
             return View();
         }
 
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
 
         public IActionResult ViewOutfit(int id)
         {
